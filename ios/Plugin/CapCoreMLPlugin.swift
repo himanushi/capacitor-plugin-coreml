b3f612289e7702bcf40b350191eb5ba4506a86ec
himanushi <himanushi@users.noreply.github.com>
2023-02-25 08:56:16 +0900

    fix

--- ios/Plugin/CapCoreMLPlugin.swift
+++ ios/Plugin/CapCoreMLPlugin.swift
@@ -1,5 +1,6 @@
 import Foundation
 import Capacitor
+import ZIPFoundation
 
 /**
  * Please read the Capacitor iOS Plugin Development Guide
@@ -13,6 +14,12 @@ public class CapCoreMLPlugin: CAPPlugin {
         let value = call.getString("value") ?? ""
         call.resolve([
             "value": implementation.echo(value)
+        ])
+    }
+
+    @objc func download(_ call: CAPPluginCall) {
+        call.resolve([
+            "value": "download"
         ])
     }
 }
