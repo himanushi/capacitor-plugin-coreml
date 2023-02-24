d24d819ef3feec00c5717728e0c299fb5d798886
himanushi <himanushi@users.noreply.github.com>
2023-02-24 09:12:06 +0900

    fix

--- src/definitions.ts
+++ src/definitions.ts
@@ -1,3 +1,23 @@
+export interface LoadOptions {
+  path: string;
+  config?: {
+    modelDisplayName?: string;
+    parameters?: Record<string, any>;
+  };
+  completionHandler: (error?: any) => void;
+}
+
+export interface PredictionOptions {
+  path: string;
+  config?: {
+    modelDisplayName?: string;
+    parameters?: Record<string, any>;
+  };
+}
+
 export interface CapCoreMLPlugin {
   echo(options: { value: string }): Promise<{ value: string }>;
+  load(options: { value: string }): Promise<any>;
+  compile(options: { value: string }): Promise<any>;
+  prediction(options: { value: string }): Promise<any>;
 }
--- src/web.ts
+++ src/web.ts
@@ -1,10 +1,22 @@
 import { WebPlugin } from '@capacitor/core';
 
-import type { CapCoreMLPlugin } from './definitions';
+import type { CapCoreMLPlugin, LoadOptions } from './definitions';
 
 export class CapCoreMLWeb extends WebPlugin implements CapCoreMLPlugin {
   async echo(options: { value: string }): Promise<{ value: string }> {
     console.log('ECHO', options);
     return options;
+  }
+
+  async load(options: { value: string }): Promise<void> {
+    console.log('LOAD', options);
+  }
+
+  async compile(options: { value: string }): Promise<void> {
+    console.log('LOAD', options);
+  }
+
+  async prediction(options: { value: string }): Promise<void> {
+    console.log('LOAD', options);
   }
 }
